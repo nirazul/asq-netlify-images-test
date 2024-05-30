ff5ba7ab33a183676b0b2cf4008834de5dd0d72a
nirazul <rouven@adorable-squid.ch>
2024-05-30 13:45:13 +0200

    Add everything elese

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+  const dyn = '/.netlify/images?url=/test.png&w100'
+</script>
+
+<template>
+  <main>
+    hello world
+    <br>
+    <br>
+    original:
+    <img style="width: 200px;" src="/test.png" alt="">
+
+    converted:
+    <img style="width: 200px;" :src="dyn" alt="">
+  </main>
+</template>
+
+<style>
+  main {
+    display: flex;
+    flex-flow: column nowrap;
+  }
+
+  img {
+    display: block;
+  }
+</style>
